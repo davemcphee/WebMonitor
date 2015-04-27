dfdcaa18a15c89235f247cd4d10150395211b2e1
Dave McPhee <alex.schmitz@gmail.com>
2015-04-27 12:52:41 -0500

    Added --init functionality Removed shelve

--- WebMon.py
+++ WebMon.py
@@ -11,16 +11,12 @@
 # v0.1 04/24/2015
 
 
-import hashlib, requests, os, shelve, sys, smtplib, configparser
+import hashlib, requests, os, sys, smtplib, configparser, argparse
 
-#bunch of bs about finding path, so 
+#bunch of bs about finding path, to help load config file from anywhere
 pathname = os.path.dirname(sys.argv[0]) 
-
-shelveFile = ''.join([os.path.abspath(pathname), '\conf\WebMon.shelve'])
-s = shelve.open(shelveFile, writeback=True)
-
 configfile = ''.join([os.path.abspath(pathname), '\conf\WebMon.config'])
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(delimiters='|')
 config.read(configfile)
 
 session = requests.Session()
@@ -28,27 +24,63 @@ session = requests.Session()
 def sendAlert(url):
 	# need to setup WebMon.config first
 	msg = 'Subject: WebMon.py update\n\n%s' % (url)
-	server = smtplib.SMTP_SSL('smtp.gmail.com:465')
-	server.login(config['WebMon']['username'], config['WebMon']['password'])
-	server.sendmail(config['WebMon']['fromaddr'], config['WebMon']['toaddr'], msg)
-	server.quit()
+	try:
+		server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+		server.login(config['WebMon']['username'], config['WebMon']['password'])
+		server.sendmail(config['WebMon']['fromaddr'], config['WebMon']['toaddr'], msg)
+		server.quit()
+	except SMTPException:
+		print('Got an error trying to send email!')
+		return 0
+	return 1
 
+def monitor():
+	for url in config['URLs']:
+		knownHash = config['URLs'][url]
+		print("Checking", url, "for any changes..")
+		r = session.get(url)
+		if (r.status_code != 200):
+			print('Unable to retrieve', url, '\nGot error code', r.status_code, 'exiting\n')
+			sys.exit()
+		newHash = hashlib.md5(bytes(r.text, 'utf-8')).hexdigest()
+		if newHash != knownHash:		# URL has changed
+			print(url, ' has changed!')
+			if sendAlert(url):			#if alert is successfully sent, update. Don't update if failure!
+					config['URLs'][url] = newHash
+					with open(configfile, 'w') as outfile:
+						config.write(outfile)
+		else:
+			print("none detected")
+			
 
-for url in s:
-	knownHash = s[url]
-	print("Checking", url, "for any changes..")
+def init(url):
 	r = session.get(url)
 	if (r.status_code != 200):
 		print('Unable to retrieve', url, '\nGot error code', r.status_code, 'exiting\n')
-		sys.exit()
+		sys.exit()	
+	
+	print('Sucesfully retrieved', url, '\nAdding to known list')
 	newHash = hashlib.md5(bytes(r.text, 'utf-8')).hexdigest()
-	if newHash != knownHash:		# URL has changed
-		print(url, ' has changed!')
-		s[url] = newHash	#update shelf
-		sendAlert(url)
+	config['URLs'][url] = newHash
+	with open(configfile, 'w') as outfile:
+		config.write(outfile)
+	
 
 
+def main():
+	parser = argparse.ArgumentParser(description='A script that monitors a list of known URLs')
+	parser.add_argument("--init", help="Add a new URL and hash to our list of knowns")
+	args = parser.parse_args()
+	
+	if args.init:
+		init(args.init)
+	else:
+		monitor()
 		
-s.close()
-print("Done.\n\n")
-		
+	
+	# if init argument given, run init and exit
+	# otherwise, run monitor and exit
+	
+
+if __name__ == "__main__" :
+		main()
